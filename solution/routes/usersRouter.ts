654d44f0426e5d43864d1908c116d0048f4cf081
Burke Holland <burkeholland@gmail.com>
2020-11-17 20:01:27 -0600

    initial commit

--- solution/routes/usersRouter.ts
+++ solution/routes/usersRouter.ts
@@ -5,7 +5,11 @@ import hbs from "../shared/hbs.ts";
 export function use(path: string, router: Router) {
 
   router.get(`${path}`, async (ctx) => {
-    ctx.response.body = await hbs.renderView("index", { title: "Oak" });
+    ctx.response.body = "Welcome User"
+  });
+
+  router.get(`${path}/:name`, async (ctx) => {
+    ctx.response.body = `Welcome ${ctx.params.name}`
   });
 
 } 
