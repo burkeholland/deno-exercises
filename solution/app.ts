b4b7f2bd465c65868d0cf94865e35650a12506f9
Burke Holland <burkeholland@gmail.com>
2020-11-22 09:09:24 -0600

    Add final solution

--- solution/app.ts
+++ solution/app.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "./deps.ts";
+import { Application, Router, send } from "./deps.ts";
 import * as indexRouter from "./routes/indexRouter.ts";
 import * as usersRouter from "./routes/usersRouter.ts";
 
@@ -8,7 +8,18 @@ const router = new Router();
 indexRouter.use("/", router);
 usersRouter.use("/users", router);
 
+// static assets
+router.get("/public/:path+", async (ctx) => {
+    await send(ctx, ctx.params.path || "", {
+        root: Deno.cwd()
+    });
+});
+
 app.use(router.routes());
+
+app.addEventListener("error", err => {
+    console.log(err);
+});
 
 console.log(`Now listening on http://0.0.0.0:3000`);
 await app.listen("0.0.0.0:3000");
