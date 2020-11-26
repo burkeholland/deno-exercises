64a5fe99293d0c2d86c7b1d7e72fc365ac0cb1ad
Burke Holland <burkeholland@gmail.com>
2020-11-26 13:45:31 -0600

    simple web server

---
+++ solution/app.ts
@@ -0,0 +1,14 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const PORT = 3000;
+const HOSTNAME = "0.0.0.0";
+
+const server = serve({ hostname: HOSTNAME, port: PORT });
+
+console.log(`Server is now running on: http://${HOSTNAME}:${PORT}`);
+
+for await (const req of server) {
+  req.respond({
+    body: "Hello World",
+  });
+}
