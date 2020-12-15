265f5bb515645e9fa33e884d4d7aefa42dc3510b
Burke Holland <burkeholland@gmail.com>
2020-12-15 09:11:54 -0600

    Add Dockerfiles

--- solution/app.ts
+++ solution/app.ts
@@ -2,6 +2,8 @@ import { Application, Router, send } from "./deps.ts";
 import * as indexRouter from "./routes/indexRouter.ts";
 import * as usersRouter from "./routes/usersRouter.ts";
 
+const PORT = Deno.env.get("PORT") || 3000;
+
 const app = new Application();
 const router = new Router();
 
@@ -21,5 +23,5 @@ app.addEventListener("error", (err) => {
   console.log(err);
 });
 
-console.log(`Now listening on http://0.0.0.0:3000`);
-await app.listen("0.0.0.0:3000");
+console.log(`Now listening on http://0.0.0.0:${PORT}`);
+await app.listen(`0.0.0.0:${PORT}`);
