8fd47ef604ec331c96532189691baad5565cd983
Burke Holland <burkeholland@gmail.com>
2021-01-03 13:09:46 -0600

    Add exercis

--- solution/app.ts
+++ solution/app.ts
@@ -1,13 +1,15 @@
-const desc = { name: "read" } as const;
+const desc = { name: "read", path: "." } as const;
 const status = await Deno.permissions.request(desc);
 
 console.log(status);
 
 if (status.state === "granted") {
-  const results = await Deno.readDir("../");
+  const results = await Deno.readDir(".");
   for await (const result of results) {
     console.log(result);
   }
 } else {
-  console.log("This program requires the 'read' permission");
+  console.log("This program requires 'read' permissions to execute");
 }
+
+export {};
